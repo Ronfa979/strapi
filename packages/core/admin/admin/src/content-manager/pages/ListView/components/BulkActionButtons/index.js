e9f7babb87e83156d57614c73c33c74353b10fd7
Simone Taeggi <startae14@gmail.com>
2023-07-07 09:52:29 +0200

    remove useless error variable

--- packages/core/admin/admin/src/content-manager/pages/ListView/components/BulkActionButtons/index.js
+++ packages/core/admin/admin/src/content-manager/pages/ListView/components/BulkActionButtons/index.js
@@ -96,7 +96,6 @@ export const ConfirmDialogPublishAll = ({
     data: countDraftRelations,
     isLoading,
     isError,
-    error,
   } = useQuery(
     ['content-manager', 'draft-relations', slug, selectedEntries],
     async () => {
